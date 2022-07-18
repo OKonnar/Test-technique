b7eb98b85fd88a5d08ff51668a987f423c25b2dc
Loic Muller <loic.muller@epitech.eu>
2022-07-18 15:50:23 +0200

    Added : Dot generation for future algorythm

--- canvas.js
+++ canvas.js
@@ -1,12 +1,21 @@
 window.addEventListener("load", () => {
+
     const canvas = document.querySelector("#canvas");
     const ctx = canvas.getContext('2d');
 
     canvas.height = window.innerHeight;
     canvas.width = window.innerWidth;
 
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    drawDot(ctx);
 })
+
+function drawDot(ctx) {
+
+    canvas.addEventListener('mousedown', (e) => {
+        ctx.fillStyle = 'black';
+        ctx.fillRect(e.x, e.y, 5, 5);
+    })
+}
 
 window.addEventListener("resize", () => {
     canvas.height = window.innerHeight;
