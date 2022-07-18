c5792c895d25706e6f349af4fef467c4042941d1
Loic Muller <loic.muller@epitech.eu>
2022-07-18 17:04:10 +0200

    Added : data structure for dot positions

--- canvas.js
+++ canvas.js
@@ -5,15 +5,22 @@ window.addEventListener("load", () => {
 
     canvas.height = window.innerHeight;
     canvas.width = window.innerWidth;
+    canvas.left = 10;
 
     drawDot(ctx);
 })
 
-function drawDot(ctx) {
+function drawDot(ctx)
+{
+    let coordinates = [];
+    let temp = [];
 
     canvas.addEventListener('mousedown', (e) => {
         ctx.fillStyle = 'black';
         ctx.fillRect(e.x, e.y, 5, 5);
+        temp = [e.x, e.y];
+        coordinates = coordinates.concat([temp]);
+        console.log(coordinates);
     })
 }
 
