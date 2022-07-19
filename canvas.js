eaea9072fa7d519656da47d310df7370bd066b3f
Loic Muller <loic.muller@epitech.eu>
2022-07-19 10:23:06 +0200

    Added : event for button and code clarification

--- canvas.js
+++ canvas.js
@@ -1,29 +1,33 @@
-window.addEventListener("load", () => {
+const canvas = document.querySelector("#canvas");
+const ctx = canvas.getContext('2d');
+const gen = document.querySelector('#gen');
+const eff = document.querySelector('#eff');
+let coordinates = [];
 
-    const canvas = document.querySelector("#canvas");
-    const ctx = canvas.getContext('2d');
+eff.addEventListener('click', () => {
+    console.log("No !");
+})
+
+gen.addEventListener('click', () => {
+    console.log("Yes !");
+})
+
+window.addEventListener("load", () => {
 
     canvas.height = window.innerHeight / 1.2;
     canvas.width = window.innerWidth / 1.1;
     ctx.fillStyle = 'grey'
     ctx.fillRect(0, 0, canvas.width, canvas.height);
-
-    drawDot(ctx);
+    ctx.fillStyle = 'black';
 })
 
-function drawDot(ctx)
-{
-    let coordinates = [];
-    let temp = [];
+canvas.addEventListener('mousedown', (e) => {
 
-    canvas.addEventListener('mousedown', (e) => {
-        ctx.fillStyle = 'black';
-        ctx.fillRect(e.x - canvas.getBoundingClientRect().left - 7, e.y - canvas.getBoundingClientRect().top - 7, 5, 5);
-        temp = [e.x, e.y];
-        coordinates = coordinates.concat([temp]);
-        console.log(coordinates);
-    })
-}
+    ctx.fillRect(e.x - canvas.getBoundingClientRect().left - 7, e.y - canvas.getBoundingClientRect().top - 7, 5, 5); // position absolue - offset canvas - offset arbitraire
+    temp = [e.x, e.y];
+    coordinates = coordinates.concat([temp]);
+    console.log(coordinates);
+})
 
 window.addEventListener("resize", () => {
     canvas.height = window.innerHeight / 1.2;
