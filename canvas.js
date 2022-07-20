c9a10134a2debfcb9261eae8eaf3bbcfcd1ceb54
Loic Muller <loic.muller@epitech.eu>
2022-07-20 17:53:21 +0200

    Update : findNextDot is now working

--- canvas.js
+++ canvas.js
@@ -41,6 +41,14 @@ window.addEventListener("resize", () => {
 
     canvas.height = window.innerHeight / 1.2;
     canvas.width = window.innerWidth / 1.1;
+
+    if (coordinates.length > 0) { // le canva fait un truc bizarre quand je resize et il y a encore des points dessus, alors je le reset
+        ctx.fillStyle = 'grey'
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+        ctx.fillStyle = 'black';
+        coordinates = [];
+        ctx.clearRect(0, 0, 1920, 1080);
+    }
 })
 
 function findNextDot (temp, coordinates) {
@@ -55,26 +63,33 @@ function findNextDot (temp, coordinates) {
         deltaCoord = deltaCoord.concat([deltaTemp]);
     }
     for (let i = 0; i != deltaCoord.length; i++) {
-        deltaFinal = deltaFinal.concat(deltaCoord[i][0] + deltaCoord[i][1]);
-        if (delta)
+        deltaFinal = deltaFinal.concat(deltaCoord[i][0] + deltaCoord[i][1]); // on additione le delta x et le delta y ce qui donne une distance (fausse, il faudrait faire thales pour avoir la vrai distance mais c'est pas utile)
+        if (i > 0 && deltaFinal[i] < deltaFinal[index])
+            index = i;
     }
-    console.log(deltaFinal);
-    console.log(Math.min(...deltaFinal));
-    ctx.beginPath();
-    ctx.moveTo(temp[0], temp[1]);
-    ctx.lineTo();
-    ctx.stroke();
+    return coordinates[index];
 }
 
 function drawShape() {
 
     const anchor = coordinates.shift();
     let temp = anchor;
+    let result = [];
 
-    findNextDot(temp, coordinates);
-    /*
-    for (let i = 0; i != 3; i++) {
+    ctx.beginPath();
+    ctx.moveTo(anchor[0], anchor[1]);
+    for (; coordinates.length > 0;) {
+        result = findNextDot(temp, coordinates);
+        temp = coordinates.shift();
+        ctx.lineTo(result[0], result[1]);
+        ctx.moveTo(result[0], result[1]);
+        ctx.stroke();
     }
+    ctx.lineTo(anchor[0], anchor[1]);
+    ctx.stroke();
+    /*
+    console.log(coordinates.length);
+    console.log(coordinates);
     ctx.beginPath();
     console.log(anchor[0], anchor[1]);
     ctx.moveTo(anchor[0], anchor[1]);
