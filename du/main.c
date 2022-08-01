c1e8ad0d9a9572e7e74b1243d979fc5daa3e4a1f
okonnor <loic.muller@epitech.eu>
2022-08-01 15:48:31 +0200

    Added : du now shows the size

--- du/main.c
+++ du/main.c
@@ -2,25 +2,36 @@
 #include <string.h>
 #include <dirent.h>
 #include <stdlib.h>
+#include <sys/stat.h>
 
-void goThroughFiles(char *path)
+int file_size(char *pathname)
 {
+    struct stat buf;
+
+    stat(pathname, &buf);
+    return (buf.st_size);
+}
+
+void getPathSize(char *path)
+{
+    char *tmpPath = path;
     char newPath[1000];
     DIR *dir = opendir(path);
+    int size = 0;
 
     if (dir == 0)
         return;
 
     for (struct dirent *dp; (dp = readdir(dir)) != NULL;) {
         if (strcmp(dp->d_name, ".") != 0 && strcmp(dp->d_name, "..") != 0) {
-            printf("%s\n", dp->d_name);
             strcpy(newPath, path);
             strcat(newPath, "/");
             strcat(newPath, dp->d_name);
-            goThroughFiles(newPath);
+            size += file_size(newPath);
+            getPathSize(newPath);
         }
     }
-
+    printf("%s %d bytes\n", tmpPath, size);
     closedir(dir);
 }
 
@@ -28,6 +39,6 @@ int main(int ac, char **av)
 {
     if (ac != 2)
         exit(84);
-    goThroughFiles(av[1]);
+    getPathSize(av[1]);
     return 0;
 }
